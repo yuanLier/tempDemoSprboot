d60ffe466e4b7974dbb088c19e74615af3b74c18
yuanyiwen <yuan15826770919@126.com>
2018-09-17 17:14:14 +0800

    首先创建一个空的springboot项目(Spring Initializr)

---
+++ src/main/java/com/demo/tempsprboot/TempsprbootApplication.java
@@ -0,0 +1,12 @@
+package com.demo.tempsprboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempsprbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempsprbootApplication.class, args);
+    }
+}
